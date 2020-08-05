6c900c61557da85db71d83e2ab80e565cebccbba
andreykokorev <andrey.kokorev.w.dev@gmail.com>
2020-08-05 13:43:08 +0700

    feat: add register form validation

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/profile">Profile</router-link>
+      <router-link to="/">Регистрация</router-link> |
+      <router-link to="/about">Сотрудники</router-link> |
+      <router-link to="/profile">Профиль</router-link>
     </div>
     <router-view/>
   </div>
--- src/views/Registration.vue
+++ src/views/Registration.vue
@@ -10,68 +10,110 @@
 						.form-group
 							label(for="name-input") Полное имя
 							input#name-input.form-control(
-								v-model="formReg.name"
+								:class="status('name')",
+								@input="$v.formReg.name.$touch()",
+								v-model="formReg.name",
 								type="text",
-								required="true",
 								aria-describedby="emailHelp"
 							)
+							.invalid-feedback(v-if="!$v.formReg.name.required") Поле обязательно для заполнения
+							.invalid-feedback(v-if="!$v.formReg.name.alpha") Используйте только символы алфавита
 
 						.form-group
 							label(for="email-input") Email
 							input#email-input.form-control(
-								v-model="formReg.email"
+								:class="status('email')",
+								@input="$v.formReg.email.$touch()",
+								v-model="formReg.email",
 								type="email",
-								required,
 								aria-describedby="emailHelp"
 							)
+							.invalid-feedback(v-if="!$v.formReg.email.required") Поле обязательно для заполнения
+							.invalid-feedback(v-if="!$v.formReg.email.email") Поле должно быть email адресом
 
 						.form-group
-							label(for="type-input", required) Тип задания
-							select#type-input.form-control(v-model="formReg.type" required )
-								option(disabled value="") Выберите один из вариантов
+							label(for="type-input") Тип задания
+							select#type-input.form-control(
+								:class="status('type')",
+								@input="$v.formReg.type.$touch()",
+								v-model="formReg.type"
+							)
+								option(disabled) Выберите один из вариантов
 								option(value="front") Frontend
 								option(value="back") Backend
+							.invalid-feedback(v-if="!$v.formReg.email.required") Поле обязательно для заполнения
 
-						button.btn.btn-primary(type="submit", @click="nextStep") Следующий шаг
+						button.btn.btn-primary.next(
+							@click="nextStep",
+							:disabled="disabledBtn",
+							type="button"
+						) Следующий шаг
 
 				transition(name="slide-fade")
 					.step(v-show="step === 2")
 						.form-group
 							label(for="github-input") Гитхаб
-							input#github-input.form-control(v-model="formReg.github" type="text")
+							input#github-input.form-control(
+								v-model="formReg.github",
+								type="text"
+							)
 
 						.form-group
 							label(for="city-input") Населённый пункт
-							input#city-input.form-control(v-model="formReg.city" type="text")
+							input#city-input.form-control(
+								:class="status('city')",
+								@input="$v.formReg.city.$touch()",
+								v-model="formReg.city",
+								type="text"
+							)
+							.invalid-feedback(v-if="!$v.formReg.city.alpha") Используйте только символы алфавита
 
 						.form-group
 							label(for="phone-input") Телефон
-							input#phone-input.form-control(v-model="formReg.phone" type="tel")
+							input#phone-input.form-control(
+								v-model="formReg.phone",
+								type="tel"
+							)
 							small Format: 999-999-999
 
 						.form-group
 							label(for="birthday-input") Дата рождения
-							input#birthday-input.form-control(v-model="formReg.birthday" type="date")
+							input#birthday-input.form-control(
+								v-model="formReg.birthday",
+								type="date"
+							)
 
-						button.btn.btn-light.mr-2(type="submit", @click="prevStep") назад
-						button.btn.btn-primary(type="submit" @click="registerUser") Зарегистрироваться
+						button.btn.btn-light.mr-2(type="button", @click="prevStep") назад
+						button.btn.btn-primary(type="submit") Зарегистрироваться
 </template>
 
 <script>
+import { required,  email, helpers } from 'vuelidate/lib/validators'
+const alpha = helpers.regex('alpha', /^[a-zA-Zа-яА-Я]*$/)
+
 export default {
 	data() {
 		return {
 			step: 1,
 			formReg: {
-				name: '',
-				email: '',
-				type: '',
-				github: '',
-				city: '',
-				phone: '',
-				birthday: ''
+				name: 'Viktor',
+				email: 'motowest12@mail.ru',
+				type: 'front',
+				github: 'https://github.com/',
+				city: 'Kemerovo',
+				phone: '88-555',
+				birthday: '28.11.1990'
 			}
 		};
+	},
+	computed: {
+		disabledBtn() {
+			return (
+				this.$v.formReg.name.$invalid ||
+				this.$v.formReg.email.$invalid ||
+				this.$v.formReg.type.$invalid
+			)
+		},
 	},
 	methods: {
 		nextStep() {
@@ -83,19 +125,73 @@ export default {
 		registerUser() {
 			console.log('Reg complete')
 			console.log(this.formReg.name)
+
+			for (const input in this.formReg) {
+				this.formReg[input] = '';
+			}
+
+			this.$v.$reset()
 		},
+		status(type) {
+			return {
+				'is-invalid warning': this.$v.formReg[type].$error
+			}
+		}
 	},
+	validations: {
+		formReg: {
+			name: {
+				required,
+				alpha
+			},
+			email: {
+				required,
+				email
+			},
+			type: {
+				required
+			},
+			city: {
+				alpha
+			}
+		}
+	}
 };
 </script>
 
 <style lang="scss" scope>
 
+.warning {
+	background: rgba(228, 25, 25, 0.11);
+}
+
+.step {
+	display: grid;
+	grid-template-columns: auto auto;
+	grid-template-rows: min-content min-content min-content auto;
+	height: 450px;
+}
+
+.form-group {
+	grid-column: 1/3;
+	align-self: start;
+}
+
+button {
+	align-self: flex-end;
+}
+
+.next {
+	grid-column: 1/3;
+}
+
 .slide-fade-enter-active {
-	transition: all .3s ease;
+	transition: all 0.3s ease;
 }
 
 .slide-fade-enter {
 	transform: translateX(10px);
 	opacity: 0;
 }
+
 </style>
